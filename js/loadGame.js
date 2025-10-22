a82079b01e2c742eea81e20f8bcaf66ddcf5e86a
coiluck <wvy.wingle@gmail.com>
2025-10-22 11:58:31 +0900

    Enable navigation from story screen to load game

--- js/loadGame.js
+++ js/loadGame.js
@@ -1,8 +1,12 @@
 // loadGame.js
-import { changeModal } from './modules/changeModal.js';
+import { changeModal, closeModal } from './modules/changeModal.js';
 
 document.querySelector('.loadgame-close-button-container').addEventListener('click', () => {
-  changeModal('top');
+  if (document.getElementById('modal-loadgame').style.zIndex === '100') {
+    closeModal('loadgame');
+  } else {
+    changeModal('top', null, 500);
+  }
 });
 
 export function initLoadGame() {
--- js/story_icons.js
+++ js/story_icons.js
@@ -1,10 +1,20 @@
 // story_icons.js
 import { changeModal, showModal, closeModal } from './modules/changeModal.js';
 
-document.getElementById('opening-icon-setting').addEventListener('click', () => {
+document.getElementById('opening-icon-save').addEventListener('click', (event) => {
+  event.stopPropagation(); 
+  // 後で書く
+});
+document.getElementById('opening-icon-load').addEventListener('click', (event) => {
+  event.stopPropagation(); 
+  showModal('loadgame');
+});
+document.getElementById('opening-icon-setting').addEventListener('click', (event) => {
+  event.stopPropagation(); 
   showModal('setting');
 });
-document.getElementById('opening-icon-menu').addEventListener('click', () => {
+document.getElementById('opening-icon-menu').addEventListener('click', (event) => {
+  event.stopPropagation(); 
   showModal('menu');
 });
 document.querySelector('.menu-close-button-container').addEventListener('click', () => {
@@ -15,7 +25,8 @@ document.getElementById('menu-item-save-data').addEventListener('click', () => {
   // あとで書く
 });
 document.getElementById('menu-item-load-data').addEventListener('click', () => {
-  // あとで書く
+  closeModal('menu');
+  showModal('loadgame');
 });
 document.getElementById('menu-item-setting').addEventListener('click', () => {
   closeModal('menu');
